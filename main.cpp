1adaa9d8ec04e849f266f16489908d9ca99bc54d
Colin McNish <newvader99@gmail.com>
2022-03-19 18:19:16 -0500

    clean up and user file input

--- main.cpp
+++ main.cpp
@@ -40,7 +40,16 @@ void sAnnealing() {
 	srand(time(0));
 	int iterationNum = 0;
 	ifstream myFile;
-	myFile.open("Program2Input.txt");
+	string userInput;
+	cout << "Enter name of file you want to open. It must be placed in the program directory!" << endl;
+	cin >> userInput;
+	myFile.open(userInput);
+	while (myFile.is_open() == false) { //Did not remember file IO at all so refered to this https://people.scs.carleton.ca/~dehne/projects/cpp-doc/tutorial/tut6-1.html#:~:text=You%20can%20check%20if%20a,open%20file%20or%20false%20otherwise.
+		cout << "file cannot be found. Place in main directory or check spelling" << endl;
+		cout << "Try again" << endl;
+		cin >> userInput;
+		myFile.open(userInput);
+	}
 	string weight;
 	string utility;
 	vector<options> opt;
@@ -64,24 +73,14 @@ void sAnnealing() {
 
 	}
 	int itemCount = 0;
-	/* testing code
-	for (int i = 0; i < opt.size(); i++) {
-
-		outputFile << "Utility " << opt.at(i).utility << " Weight: " << opt.at(i).weight << " Selected? " << opt.at(i).isSelected << "\n";
-		itemCount++;
-	}
-	*/
-
-
 	initialRandom20(opt);
 
 	outputFile << "initial utility from first random twenty items is " << getTotalUtility(opt) << "\n";
 
 	while (numRejections != 40000) { //if 40000 attempts occur with no changes we are all good to report...
 
-		
 		if (numchanges == 4000 || numAttempts == 40000) {
-			temp = temp * .92;
+			temp = temp * .9;
 			if (temp < 0) {
 				temp = 0;
 			}
@@ -102,23 +101,18 @@ void sAnnealing() {
 				numRejections = 0;
 			}
 		}
+
 		float distToMin = 1000 - getTotalUtility(opt);
 		float totalUtilBeforeNewOption = getTotalUtility(opt);
 		float weightBeforeNewOption = getTotalWeight(opt);
 
 		int randIndex = rand() % 399 + 1; //randomly select new item
 
-
-
-
-
 		float utilityOfNewItem = opt.at(randIndex).utility;
 		float weightOfNewItem = opt.at(randIndex).weight;
 
-
 		float totalUtilAfterNewOption = 0;
 		float weightAfterNewOption = 0;
-
 
 		if (opt.at(randIndex).isSelected == true) { //item already selected so will be unselected
 			opt.at(randIndex).isSelected = false; //testing new option
@@ -127,16 +121,8 @@ void sAnnealing() {
 
 			if (weightAfterNewOption > 500) {
 				utilityOfNewItem = utilityOfNewItem - (20 * (weightAfterNewOption - 500)); // need to add penalty to item if it takes us over our total weight
-
-				if (iterationNum % 40000 == 0) {
-					outputFile << "Weight is " << weightAfterNewOption << "\n";
-				}
-
 			}
-
-
-			totalUtilAfterNewOption = totalUtilBeforeNewOption - utilityOfNewItem; //need absolute value here for negatives?
-
+			totalUtilAfterNewOption = totalUtilBeforeNewOption - utilityOfNewItem;
 		}
 
 		else { // item was not already selected so we will try selecting it
@@ -144,39 +130,22 @@ void sAnnealing() {
 			weightAfterNewOption = weightBeforeNewOption + weightOfNewItem;
 			if (weightAfterNewOption > 500) {
 				utilityOfNewItem = utilityOfNewItem - (20 * (weightAfterNewOption - 500)); // need to add penalty to item if it takes us over our total weight
-
-				if (iterationNum % 40000 == 0) {
-					//outputFile << "Weight is " << weightAfterNewOption << "\n";
-				}
 			}
-
-
 			totalUtilAfterNewOption = totalUtilBeforeNewOption + utilityOfNewItem;
-
 		}
-
-
-
-
-
 
 		if ((1000 - totalUtilAfterNewOption) < (1000 - totalUtilBeforeNewOption)) {
 			numchanges += 1;
-			numAttempts++; //reset attempts if we actually successfully change something
+			numAttempts++; 
 			numRejections = 0;
 
 		}
 
 		else {
-
-			//opt.at(randIndex).isSelected = false;
-
 			float changeInE = ((1000 - totalUtilAfterNewOption) - (1000 - totalUtilBeforeNewOption));
 
 			float probToBeSelectedAnyway = float(exp(-(changeInE / temp)));
 			float randomValue = (rand() / (float)RAND_MAX);
-			//outputFile << "Change in E is " << changeInE << endl;
-			//outputFile << "prob select anyway " << probToBeSelectedAnyway << " rand value" << randomValue <<  "\n";
 			bool selection;
 			if (probToBeSelectedAnyway > randomValue) {
 				selection = true;
@@ -185,14 +154,9 @@ void sAnnealing() {
 				selection = false;
 			}
 
-
-
 			if (selection == true) {
-				//opt.at(randIndex).isSelected = !(opt.at(randIndex).isSelected);
-				//numAttempts = 0;
 				numchanges++;
 				numAttempts++;
-				//numRejections = 0; Not needed?
 			}
 			else {
 				opt.at(randIndex).isSelected = !opt.at(randIndex).isSelected; //decided not worth keeping change so reverse it
@@ -200,22 +164,8 @@ void sAnnealing() {
 				numRejections++;
 			}
 		}
-
-
-
-
-		//options.at(randIndex).isSelected = true;
-
-		if (iterationNum % 40000 == 0) {
-			//outputFile << "Iteration:" << iterationNum << " " << "Num changes:" << numchanges << " " << "Num attempts:" << numAttempts << "Utility Total " << getTotalUtility(opt) << "current temp " << temp << "num rejections " << numRejections << "weight " << getTotalWeight(opt) << "\n";
-		}
 		iterationNum++;
 	}
-
-
-
-
-
 	itemCount = 0;
 
 	for (int i = 0; i < opt.size(); i++) {
@@ -223,14 +173,11 @@ void sAnnealing() {
 			itemCount++;
 		}
 	}
-
-
-
-
-
 	cout << "Total number of iterations was " << iterationNum << " Total items packed is " << itemCount << " Total utility is " << getTotalUtility(opt) << " total weight is " << getTotalWeight(opt) << endl;
 
 	outputFile << "Total number of iterations was " << iterationNum << " Total items packed is " << itemCount << " Total utility is " << getTotalUtility(opt) << " total weight is " << getTotalWeight(opt) << endl;
+	myFile.close();
+	outputFile.close();
 }
 
 int main() {
